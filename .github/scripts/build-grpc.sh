d3e284aed336aaf157f093b2e11272092adae594
Joshua Chapman <joshua.chapman@secomind.com>
2025-06-23 11:57:13 +0200

    ci(grpc): improve the build of gRPC and allow dynamic libs
    Signed-off-by: Joshua Chapman <joshua.chapman@secomind.com>

---
+++ .github/scripts/build-grpc.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+# (C) Copyright 2025, SECO Mind Srl
+#
+# SPDX-License-Identifier: Apache-2.0
+
+set -exEuo pipefail
+
+cmake -S ./grpc/ -B ./grpc/cmake/build -G Ninja \
+    -DBUILD_SHARED_LIBS=ON \
+    -DgRPC_INSTALL=ON \
+    -DgRPC_BUILD_TESTS=OFF \
+    -DCMAKE_CXX_STANDARD=17 \
+    -DCMAKE_INSTALL_PREFIX="$MY_INSTALL_DIR"
+
+cmake --build ./grpc/cmake/build
+
+cmake --install ./grpc/cmake/build
